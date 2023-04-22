0f4840d9dc018fa4b3734f7f76c1afb16ca758d9
Julian Bopp <julian.bopp@tutanota.com>
2023-04-22 16:10:29 +0200

    Add template for GPmodel

---
+++ project/GPmodel.scala
@@ -0,0 +1,23 @@
+//> using scala "3.1.2"
+//> using lib "ch.unibas.cs.gravis::scalismo-ui:0.91.0"
+import scalismo.geometry._
+import scalismo.common._
+import scalismo.common.interpolation.TriangleMeshInterpolator3D
+import scalismo.mesh._
+import scalismo.io.{StatisticalModelIO, MeshIO}
+import scalismo.statisticalmodel._
+import scalismo.numerics.UniformMeshSampler3D
+import scalismo.kernels._
+
+import scalismo.ui.api._
+
+import breeze.linalg.{DenseMatrix, DenseVector}
+
+object GPmodel extends App {
+
+    scalismo.initialize()
+    implicit val rng: scalismo.utils.Random = scalismo.utils.Random(42)
+
+    val ui = ScalismoUI()
+  
+}
