0ec4ea1f1cac90ad87a973dbd9e962e4899ad834
Julian Bopp <julian.bopp@tutanota.com>
2023-04-24 16:48:13 +0200

    Change to 46 samples

--- project/RigidAlignment.scala
+++ project/RigidAlignment.scala
@@ -44,7 +44,7 @@ object RigidAlignment extends App {
   val resultDir = new java.io.File("results/aligned")
   resultDir.mkdirs()
 
-  val fileIds = 0 until 46 // change to 46 to process all the files
+  val fileIds = 0 until 47 // change to 47 to process all the files
 
   val alignedGroup = ui.createGroup("aligned meshes")
 
